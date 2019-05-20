eb7d49a9c463f4e933d099b5f608a60655e90ad1
Péter Gyula <gyulap01@gmail.com>
2019-05-20 21:26:09 +0200

    Add files via upload

--- sRNA_analysis.sh
+++ sRNA_analysis.sh
@@ -106,7 +106,7 @@ if [[ ! -f $bamfile ]]; then
   while read rg
     do
       mappedname="${mappedout}/${rg%_trimmed}_trimmed_mapped.fastq.gz"
-      samtools view -bu -F4 -r $rg $bamfile | samtools fastq - > $mappedname
+      samtools view -bu -F4 -r $rg $bamfile | samtools fastq - | pigz -p $p > $mappedname
       fastqc --extract -t $p -o "${mappedout}/FastQC_${mappedout##*/}" $mappedname
       fastqcdata="${mappedout}/FastQC_${mappedout##*/}/${mappedname%.fastq.gz}_fastqc/fastqc_data.txt"
       awk -v rg="$rg" 'BEGIN{FS=OFS="\t"}NR==7{print rg, (1000000/$2)}' $fastqcdata >> "${ShortStackout}/norm_factors.txt"
