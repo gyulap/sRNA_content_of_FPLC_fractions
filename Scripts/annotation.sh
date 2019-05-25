b3a28e5ccee76ed159f2070f9a0562b25904677c
Péter Gyula <gyulap01@gmail.com>
2019-05-25 09:36:59 +0200

    Add files via upload

--- Scripts/annotation.sh
+++ Scripts/annotation.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/zsh
 
-outdir='./sRNA-seq'
+wd=$PWD
+outdir="${wd}/sRNA-seq"
 
-miRBase='./Auxiliary_files/miRBase_mature_sequences.fasta'
-tasiRNA='./Auxiliary_files/tasiRNA_sequences.fasta'
-TAIR10='./Auxiliary_files/TAIR10_genes_intergenic_merged.fasta'
+miRBase="${wd}/Auxiliary_files/miRBase_mature_sequences.fasta"
+tasiRNA="${wd}/Auxiliary_files/tasiRNA_sequences.fasta"
+TAIR10="${wd}/Auxiliary_files/TAIR10_genes_intergenic_merged.fasta"
 pattern="${outdir}/Top_5000_sequences.fasta"
 
 cd $outdir
 
-patman -D $miRbase -P $pattern -e 0 -s > 'miRBase.patman'
+patman -D $miRBase -P $pattern -e 0 -s > 'miRBase.patman'
 patman -D $tasiRNA -P $pattern -e 0 -s > 'tasiRNA.patman'
 patman -D $TAIR10 -P $pattern -e 1 -s > 'TAIR10.patman'
 
 annot(){
-       awk -v name="$1" 'BEGIN{FS=OFS="\t"}
+       d=$1
+       p=$2
+       awk -v name="$d" 'BEGIN{FS=OFS="\t"}
        NR==FNR{a[$2]=a[$2]";
                "$1"("$3"-"$4","$5", mm: "$6")";
                next
@@ -24,8 +27,8 @@ annot(){
                else if ($2 in a)
                   {print $0, a[$2]}
                else {print $0, "No hit"}
-              }' "${1}.patman" $2 |\
-       sed 's/\t[;]/\t/' > "${$2%.txt}_${$1}.txt"
+              }' "${d}.patman" $p |\
+       sed 's/\t[;]/\t/' > "${p%.txt}_${d}.txt"
        }
 
 annot 'miRBase' 'Top_5000_sequences.txt'
@@ -34,4 +37,4 @@ annot 'TAIR10' 'Top_5000_sequences_miRBase_tasiRNA.txt'
 
 rm -f 'Top_5000_sequences_miRBase.txt' 'Top_5000_sequences_miRBase_tasiRNA.txt' *.patman
 
-cd ..
+cd $wd
